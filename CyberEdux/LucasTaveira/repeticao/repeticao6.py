e50c79dcd1989a058338f05ab0394c943fac3bfc
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2025-02-17 00:49:40 -0400

    fix: corrigido exercicio 6

--- CyberEdux/LucasTaveira/repeticao/repeticao6.py
+++ CyberEdux/LucasTaveira/repeticao/repeticao6.py
@@ -1,17 +1,9 @@
-valor_n = int(input("Digite o valor de N: "))
-i = int(1)
-while i <= valor_n:
-    if i == 1 or i == valor_n:
-        print("*" * valor_n)
-    elif i > 3 and i <= (valor_n - 3) and i != valor_n / 2:
-        print("*", " " * 0, "*", " " * 0, "*", " " * 0, "*")
-    elif i >= valor_n / 2 and i < (valor_n - 3):
-        print("*", " " * 1, "**", " " * 1, "*")
-        if valor_n % 2 == 0:
-            print("*", " " * 1, "**", " " * 1, "*")
-        i += 1
-    elif (i == 2 and i != valor_n) or (i == valor_n - 1 and i != valor_n):
-        print("**", " " * (valor_n - 6), "**")
-    elif (i == 3 and i != valor_n) or (i == valor_n - 2 and i != valor_n):
-        print("* *", " " * (valor_n - 8), "* *")
-    i += 1
+N = int(input("Digite o valor de N: "))
+for i in range(N):
+    linha = ""
+    for j in range(N):
+        if i == 0 or i == N - 1 or j == 0 or j == N - 1 or i == j or i + j == N - 1:
+            linha += "*"
+        else:
+            linha += " "
+    print(linha)
